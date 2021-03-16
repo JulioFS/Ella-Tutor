5220ef3bfc27c328dab3f3b41f843f30b80cbf4b
Julio F Schwarzbeck <julio@techfuel.net>
2021-03-15 23:10:58 -0700

    First working version, no error checking

--- frontend/school.js
+++ frontend/school.js
@@ -1,8 +1,9 @@
-var School = (function () {
-    let initialize = function () {
+var School = (() => {
+    let initialize = () => {
         /* Create Events */
         $('#submit-code').on('click', handleLogin);
         $('#logout').on('click', handleLogout);
+        //$('#selected-courses input[type=button]').on('click', removeCourse);
     };
 
     /* 
@@ -10,56 +11,110 @@ var School = (function () {
      * {endpoint: '', method: ''}
      * Example { endPoint: '/student/1', method: 'GET' }
     */
-   let queryAPI = function (opts) {
-       /* Returns the standard ajax promise object, use .then() and .fail()
-        * to handle it in your calling function/method
-        */
-       let ajaxOpts = {};
+    let queryAPI = (opts) => {
+        /* Returns the standard ajax promise object, use .then() and .fail()
+         * to handle it in your calling function/method
+         */
+        let ajaxOpts = {};
 
-       /* This is standard AJAX "stuff", define the object containing
-        * the information to pass to the Python process..
-        */
-       ajaxOpts = {
-           url: 'http://127.0.0.1:5000' + opts.endPoint,
-           method: opts.method,
-           dataType: 'json',
-           beforeSend: function (jqXHR) {
-               jqXHR.setRequestHeader('Content-Type', 'application/json');
-           }
-       };
-       return $.ajax(ajaxOpts);
-   };
+        /* This is standard AJAX "stuff", define the object containing
+         * the information to pass to the Python process..
+         */
+        ajaxOpts = {
+            url: 'http://127.0.0.1:5000' + opts.endPoint,
+            method: opts.method,
+            dataType: 'json',
+            beforeSend: function (jqXHR) {
+                jqXHR.setRequestHeader('Content-Type', 'application/json');
+            }
+        };
+        return $.ajax(ajaxOpts);
+    };
 
-   let handleLogin = function (event) {
-       let student_code = $('#student-code').val();
-       if (student_code.length > 0) {
-           queryAPI({ endPoint: '/students/login/' + student_code, method: 'GET' }).then(function (results) {
-               if (results) {
-                    STUDENT = {name: results.name, id: results.id};
+    let handleLogin = (event) => {
+        let student_code = $('#student-code').val();
+        if (student_code.length > 0) {
+            queryAPI({ endPoint: `/students/login/${student_code}`, method: 'GET' }).then(function (results) {
+                if (results) {
+                    STUDENT = { name: results.name, id: results.id };
                     $('#student-name').html(results.name);
                     $('#content').show();
                     $('#login').hide();
                     $('#success-login').show();
-               } else {
-                   alert('Student Not Found. Please Try Again.');
-               }
-           }).fail(function (err) {
-               alert('Error executing AJAX, is the backend server running?');
-           });
-       } else {
-           alert('Please enter a valid student code value.')
-       }
-       return false;
-   };
+                    showSubscriptions();
+                } else {
+                    alert('Student Not Found. Please Try Again.');
+                }
+            }).fail(function (err) {
+                alert('Error executing AJAX, is the backend server running?');
+            });
+        } else {
+            alert('Please enter a valid student code value.')
+        }
+        return false;
+    };
 
-   let handleLogout = function(event) {
-       STUDENT = {};
-       $('#content').hide();
-       $('#login').show();
-       $('#success-login').hide();
-   };
+    let handleLogout = (event) => {
+        STUDENT = {};
+        $('#content').hide();
+        $('#login').show();
+        $('#success-login').hide();
+    };
+
+    let showSubscriptions = () => {
+        /* Populate Subscriptions */
+        $('#selected-courses').empty();
+        STUDENT.courses = [];
+        /* Keep tally of currently taken courses so the student does not choose the same course twice */
+        queryAPI({ endPoint: `/students/courses/${STUDENT.id}`, method: 'GET' }).then((courses) => {
+            if (courses) {
+                courses.forEach(element => {
+                    $('#selected-courses').append(
+                        `<tr><td>${element.course_code}</td><td>${element.course_name}</td><td>${element.course_time}</td><td><input type="button" value="Remove" onclick="School.handleCourse(this)" data-action-id="remove" data-course-id="${element.course_id}"></td></tr>`);
+                    STUDENT.courses.push(element.course_id);
+                });
+                showCourses();
+            }
+        }).fail((err) => {
+            alert('Error executing AJAX, is the backend server running?');
+        });
+    };
+
+    let showCourses = () => {
+        /* Populate all courses section */
+        $('#available-courses').empty();
+        queryAPI({ endPoint: '/courses', method: 'GET' }).then((courses) => {
+            if (courses) {
+                let buttonState = '';
+                courses.forEach(element => {
+                    if (STUDENT.courses.indexOf(element.id) >= 0) {
+                        buttonState = 'disabled';
+                    } else {
+                        buttonState = '';
+                    }
+                    $('#available-courses').append(
+                        `<tr><td>${element.course_id}</td><td>${element.name}</td><td>${element.course_time}</td><td><input type="button" value="Add" onclick="School.handleCourse(this)" data-action-id="add" data-course-id="${element.id}" ${buttonState}></td></tr>`);
+                });
+            } else {
+                alert('Courses Not Found. Please Try Again Later.');
+            }
+        }).fail((err) => {
+            alert('Error executing AJAX, is the backend server running?');
+        });
+    };
+
+    let handleCourse = (elem) => {
+        let courseId = $(elem).data('courseId');
+        let action = $(elem).data('actionId');
+        queryAPI({ endPoint: `/students/${STUDENT.id}/course/${courseId}/${action}`, method: 'POST' }).then(() => {
+            showSubscriptions();
+        }).fail((err) => {
+            alert('Error executing AJAX, is the backend server running?');
+        });
+    };
 
     return {
-        initialize: initialize
+        initialize: initialize,
+        handleCourse: handleCourse
     };
-}());
+})();
